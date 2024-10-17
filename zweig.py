8931ab94809bd0f8d8470bef613f26defb0f682f
Amancham <amancham@gmx.net>
2024-10-17 13:44:11 +0200

    wurzel branch created

--- zweig.py
+++ zweig.py
@@ -1,10 +1,10 @@
-# This is the main-branch 
+# This is the wurzel-branch 
 
 def main():
 	#define branch_name variable
-	branch_name = "main"
+	branch_name = "wurzel"
 	# print the branch_name variable inside a 'Python-format-string'
-	print(f"This is the {branch_name}-branch of the baum:")
+	print(f"This is the {branch_name}-branch of the baum.")
 
 if __name__ == "__main__":
 	main()
