cb2ef1247c685a57fd9be7f474734cd682195c8a
Amancham <amancham@gmx.net>
2024-10-17 13:51:28 +0200

    created ast

--- zweig.py
+++ zweig.py
@@ -1,8 +1,8 @@
-# This is the wurzel-branch 
+# This is the ast-branch 
 
 def main():
 	#define branch_name variable
-	branch_name = "wurzel"
+	branch_name = "ast"
 	# print the branch_name variable inside a 'Python-format-string'
 	print(f"This is the {branch_name}-branch of the baum.")
 
