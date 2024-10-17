ec98ba22821330a3e7eae51ea4e4082e60aee01a
Amancham <amancham@gmx.net>
2024-10-17 08:22:00 +0200

    main created

---
+++ zweig.py
@@ -0,0 +1,10 @@
+# This is the main-branch 
+
+def main():
+	#define branch_name variable
+	branch_name = "main"
+	# print the branch_name variable inside a 'Python-format-string'
+	print(f"This is the {branch_name}-branch of the baum:")
+
+if __name__ == "__main__":
+	main()
